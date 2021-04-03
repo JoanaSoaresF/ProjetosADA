88aa77f26d411c172eed88d6fdb4daa297ba1846
Joana <js.faria@campus.fct.unl.pt>
2021-04-03 20:08:39 +0100

    Esboço da implementação com matriz tridimensional

--- 1stProject(GameOfBeans)/GameOfBeans.java
+++ 1stProject(GameOfBeans)/GameOfBeans.java
@@ -1,15 +1,11 @@
+public class GameOfBeans {
 
-
-public class GameOfBeans{
-
-    //MARK: Variables
-
-    private int[][] bestScores;
+    private int[][][] bestScores;
     private int depth;
     private int[] piles;
     private String firstPlayer;
-    private static final boolean JABA = true;
-    private static final boolean PIETON = false;
+    private static final int JABA = 0;
+    private static final int PIETON = 1;
     private static final int LEFT = 0;
     private static final int RIGHT = 1;
 
@@ -18,34 +14,34 @@ public class GameOfBeans{
         this.depth = depth;
         this.piles = piles;
         this.firstPlayer = firstPlayer;
-        bestScores = new int[piles.length][piles.length];
+        bestScores = new int[piles.length][piles.length][2];
     }
 
-    private int[] pietonPlay(int i, int j){
+    private int[] pietonPlay(int i, int j) {
         int maxScore = Integer.MIN_VALUE;
         int[] answer = {0, 0};
 
-        for(int k = 1; k <= depth && (i + k <= j); k++) {
+        for (int k = 1; k <= depth && (i + k <= j); k++) {
             int sum = 0;
 
-            for(int c = 0; c < k; c++){
+            for (int c = 0; c < k; c++) {
                 sum += piles[i + c - 1];
             }
 
-            if(sum > maxScore){
+            if (sum > maxScore) {
                 maxScore = sum;
                 answer = new int[]{k, 0};
             }
         }
 
-        for(int k = 1; k <= depth && (j - k >= i); k++) {
+        for (int k = 1; k <= depth && (j - k >= i); k++) {
             int sum = 0;
 
-            for(int c = 0; c < k; c++){
+            for (int c = 0; c < k; c++) {
                 sum += piles[j - c - 1];
             }
 
-            if(sum > maxScore){
+            if (sum > maxScore) {
                 maxScore = sum;
                 answer = new int[]{0, k};
             }
@@ -58,12 +54,12 @@ public class GameOfBeans{
     private int score(int k, int i, int j) {
         int score = 0;
 
-        if(k<0){
-            for(int counter  = 0; counter < k; counter++){
+        if (k < 0) {
+            for (int counter = 0; counter < -k; counter++) {
                 score += piles[j - 1 - counter];
             }
         } else {
-            for(int counter  = 0; counter < k; counter++){
+            for (int counter = 0; counter < k; counter++) {
                 score += piles[i - 1 + counter];
             }
         }
@@ -72,47 +68,43 @@ public class GameOfBeans{
     }
 
 
-
     public int bestJabaScore() {
-        boolean player = firstPlayer.toLowerCase().equals("jaba");
+        int player = firstPlayer.toLowerCase().equals("jaba") ? JABA : PIETON;
         //base cases = only one pile left (i=j)
-        for(int i = 1; i<piles.length; i++){
-            bestScores[i][i] = piles[i-1];
+        for (int i = 1; i < piles.length; i++) {
+            bestScores[i][i][PIETON] = piles[i - 1];
+            bestScores[i][i][JABA] = 0;
         }
 
         //general case. P is the difference of indices of piles
-        for(int p = 1; p<piles.length;p++){
-            for(int i = 1; i<piles.length-p; i++){ //i is the left index
-                int j = i+p; //j is the right index
-                int maxScore = Integer.MIN_VALUE;
-                int maxToRemove = Integer.min(depth, p+1);
-                if(player == JABA){
-                    //it is Jaba's turn to play
-                    for(int k = 1; k <=maxToRemove; k++){ //k is the number of piles to remove
-                        int score = score(k, i, j) + bestScores[i-k][j];
-                        if(score > maxScore){
-                            maxScore = score;
-                        }
+        for (int p = 1; p < piles.length; p++) {
+            for (int i = 1; i < piles.length - p; i++) { //i is the left index
+                int j = i + p; //j is the right index
+                int maxScoreJaba = Integer.MIN_VALUE;
+                int maxToRemove = Integer.min(depth, p + 1);
 
-                    }
-
-                } else {
-                    //it is Pieton's turn to play
-                    int[] play = pietonPlay(i, j);
-                    maxScore = bestScores[i-play[LEFT]][j-play[RIGHT]];
-
+                //it is Jaba's turn to play
+                for (int k = 1; k <= maxToRemove; k++) { //k is the number of piles to remove
+                    //remove from left
+                    int scoreLeft = score(k, i, j) + bestScores[i - k][j][PIETON];
+                    maxScoreJaba = Integer.max(scoreLeft, maxScoreJaba);
+                    //remove from right
+                    int scoreRight = score(-k, i, j) + bestScores[i][j - k][PIETON];
+                    maxScoreJaba = Integer.max(scoreRight, maxScoreJaba);
                 }
-                //player turn changes
-                player = !player;
-                bestScores[i][j] = maxScore;
+
+                bestScores[i][j][JABA] = maxScoreJaba;
+
+                //it is Pieton's turn to play
+                int[] play = pietonPlay(i, j);
+                int maxScorePieton = bestScores[i - play[LEFT]][j - play[RIGHT]][JABA];
+                bestScores[i][j][JABA] = maxScorePieton;
 
             }
 
         }
-
         //solution is the best possible way for Jaba to score with the piles from 1 to the last
-        return bestScores[1][piles.length];
+        return bestScores[1][piles.length][player];
     }
-
 
 }
