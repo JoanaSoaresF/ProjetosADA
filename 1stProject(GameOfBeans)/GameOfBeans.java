d45d4b4bd2a47f3a07871637677cc6cbd19a96a5
Joana <js.faria@campus.fct.unl.pt>
2021-04-06 09:26:37 +0100

    Accepted

--- 1stProject(GameOfBeans)/GameOfBeans.java
+++ 1stProject(GameOfBeans)/GameOfBeans.java
@@ -24,14 +24,23 @@ public class GameOfBeans {
         bestScores = new int[piles.length + 1][piles.length + 1][2];
     }
 
+    /**
+     * Computes  Pieton's play.
+     *
+     * @param i left index(-1) of the pile
+     * @param j right index(-1) of the pile
+     * @return the number of piles removed from which side
+     */
     private int[] pietonPlay(int i, int j) {
         int maxScore = Integer.MIN_VALUE;
         int[] answer = {0, 0};
 
-        for (int k = 1; k <= depth && (i + k <= j + 1); k++) {
+        //left play
+        for (int k = 1; k <= depth && (i + k <= j + 1); k++) { // k is the number of piles to remove
             int sum = 0;
 
-            for (int c = 0; c < k; c++) {
+            //TODO change to score
+            for (int c = 0; c < k; c++) { // c is a counter to sum all the piles to remove
                 sum += piles[i + c - 1];
             }
 
@@ -41,9 +50,12 @@ public class GameOfBeans {
             }
         }
 
+        //right play
         for (int k = 1; k <= depth && (j - k + 1 >= i); k++) {
             int sum = 0;
 
+            //TODO change to score
+            //sum = score(k, i, j)
             for (int c = 0; c < k; c++) {
                 sum += piles[j - c - 1];
             }
@@ -57,7 +69,16 @@ public class GameOfBeans {
         return answer;
     }
 
-    // k>0 means we should augment i, k<0 means we should move j backwards
+
+    /**
+     * Computes the Jabas's score from removing k piles from the piles i to j
+     * k>0 means we should augment i, k<0 means we should move j backwards
+     *
+     * @param k piles to remove
+     * @param i left index(-1) of the pile
+     * @param j right index(-1) of the pile
+     * @return the score from removing k piles
+     */
     private int score(int k, int i, int j) {
         int score = 0;
 
@@ -74,7 +95,11 @@ public class GameOfBeans {
         return score;
     }
 
-
+    /**
+     * Computes the max score for Jaba, solves the problem
+     *
+     * @return the maximum Jaba score
+     */
     public int bestJabaScore() {
         int player = firstPlayer.equalsIgnoreCase("jaba") ? JABA : PIETON;
         //base cases = only one pile left (i=j)
@@ -84,19 +109,23 @@ public class GameOfBeans {
         }
 
         //general case. P is the difference of indices of piles
-        for (int p = 1; p <= piles.length; p++) {
+        for (int p = 1; p < piles.length; p++) {
             for (int i = 1; i <= piles.length - p; i++) { //i is the left index
                 int j = i + p; //j is the right index
                 int maxScoreJaba = Integer.MIN_VALUE;
-                int maxToRemove = Integer.min(depth, p);
+                int maxToRemove = Integer.min(depth, p + 1);
 
                 //it is Jaba 's turn to play
                 for (int k = 1; k <= maxToRemove; k++) { //k is the number of piles to remove
                     //remove from left
-                    int scoreLeft = score(k, i, j) + bestScores[i + k][j][PIETON];
+                    int scoreLeft = score(k, i, j);
+                    if (k + i <= piles.length)// does not empty the piles
+                        scoreLeft += bestScores[i + k][j][PIETON];
                     maxScoreJaba = Integer.max(scoreLeft, maxScoreJaba);
                     //remove from right
-                    int scoreRight = score(-k, i, j) + bestScores[i][j - k][PIETON];
+                    int scoreRight = score(-k, i, j);
+                    if (j - k > 0)// does not empty the piles
+                        scoreRight += bestScores[i][j - k][PIETON];
                     maxScoreJaba = Integer.max(scoreRight, maxScoreJaba);
                 }
 
@@ -104,8 +133,11 @@ public class GameOfBeans {
 
                 //it is Pieton 's turn to play
                 int[] play = pietonPlay(i, j);
-                //FIXME Index 3 out of bounds for length 3
-                int maxScorePieton = bestScores[i + play[LEFT]][j - play[RIGHT]][JABA];
+                int maxScorePieton = 0; //Jaba's best score if is Pieton's play
+                //does not empty the piles
+                if (play[LEFT] + i <= piles.length && j - play[RIGHT] > 0) {
+                    maxScorePieton = bestScores[i + play[LEFT]][j - play[RIGHT]][JABA];
+                }
                 bestScores[i][j][PIETON] = maxScorePieton;
 
             }
